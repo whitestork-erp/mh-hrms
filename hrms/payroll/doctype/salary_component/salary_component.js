c6bde225267703653118e33d06b02b0bb762f445
El-Shafei H <el.shafei.developer@gmail.com>
2025-08-21 09:42:08 +0300

    fix(Salary Component): add missing translation function
    (cherry picked from commit 2f8e6a9f5b1fdfad09facc24b939fb5c0b1e45ca)
    
    # Conflicts:
    #	hrms/payroll/doctype/salary_component/salary_component.js

--- hrms/payroll/doctype/salary_component/salary_component.js
+++ hrms/payroll/doctype/salary_component/salary_component.js
@@ -56,6 +56,83 @@ frappe.ui.form.on("Salary Component", {
 			frm.set_value("create_separate_payment_entry_against_benefit_claim", 0);
 		}
 	},
+<<<<<<< HEAD
+=======
+
+	add_update_structure_button: function (frm) {
+		for (const df of ["Condition", "Formula"]) {
+			frm.add_custom_button(
+				__("Sync {0}", [__(df)]),
+				function () {
+					frappe
+						.call({
+							method: "get_structures_to_be_updated",
+							doc: frm.doc,
+						})
+						.then((r) => {
+							if (r.message.length)
+								frm.events.update_salary_structures(frm, df, r.message);
+							else
+								frappe.msgprint({
+									message: __(
+										"Salary Component {0} is currently not used in any Salary Structure.",
+										[frm.doc.name.bold()],
+									),
+									title: __("No Salary Structures"),
+									indicator: "orange",
+								});
+						});
+				},
+				__("Update Salary Structures"),
+			);
+		}
+	},
+
+	update_salary_structures: function (frm, df, structures) {
+		let msg = __("{0} will be updated for the following Salary Structures: {1}.", [
+			__(df),
+			frappe.utils.comma_and(
+				structures.map((d) =>
+					frappe.utils.get_form_link("Salary Structure", d, true).bold(),
+				),
+			),
+		]);
+		msg += "<br>";
+		msg += __("Are you sure you want to proceed?");
+		frappe.confirm(msg, () => {
+			frappe
+				.call({
+					method: "update_salary_structures",
+					doc: frm.doc,
+					args: {
+						structures: structures,
+						field: df.toLowerCase(),
+						value: frm.get_field(df.toLowerCase()).value || "",
+					},
+				})
+				.then((r) => {
+					if (!r.exc) {
+						frappe.show_alert({
+							message: __("Salary Structures updated successfully"),
+							indicator: "green",
+						});
+					}
+				});
+		});
+	},
+
+	create_salary_structure: function (frm) {
+		frappe.model.with_doctype("Salary Structure", () => {
+			const salary_structure = frappe.model.get_new_doc("Salary Structure");
+			const salary_detail = frappe.model.add_child(
+				salary_structure,
+				frm.doc.type === "Earning" ? "earnings" : "deductions",
+			);
+			salary_detail.salary_component = frm.doc.name;
+			frappe.set_route("Form", "Salary Structure", salary_structure.name);
+		});
+	},
+>>>>>>> 2f8e6a9f (fix(Salary Component): add missing translation function)
 });
 
 var set_value_for_condition_and_formula = function (frm) {
